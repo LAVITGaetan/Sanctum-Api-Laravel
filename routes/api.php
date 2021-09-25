c0cbbd2cf917d2b07d556a10bde18f9c50556451
Gaetan LAVIT <g.lavit@outlook.fr>
2021-09-25 12:36:25 +0400

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+// Public Routes
+
+Route::resource('products', ProductController::class);
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+});
+
+//Route::get('/products', [ProductController::class, 'index']);
+//Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
