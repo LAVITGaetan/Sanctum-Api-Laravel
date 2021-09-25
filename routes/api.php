98de4b44a3352cbdff48e3c4104930dbc36d2d86
Gaetan LAVIT <g.lavit@outlook.fr>
2021-09-25 14:05:55 +0400

    Ajout du systeme d authentification avec token

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,15 +19,26 @@ use Illuminate\Support\Facades\Route;
 
 // Public Routes
 
-Route::resource('products', ProductController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/products', [ProductController::class, 'store']);
+
+
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('products/search/{name}', [ProductController::class, 'search']);
 
 // Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-Route::get('products/search/{name}', [ProductController::class, 'search']);
-});
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::post('/products', [ProductController::class, 'store']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
